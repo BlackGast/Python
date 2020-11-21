dd705cbe52789f6eba67e7f32cc113a9cb25bafc
BlackGast <56234047+BlackGast@users.noreply.github.com>
2020-11-21 12:40:42 +0500

    21.11

--- 2.py
+++ 2.py
@@ -1,6 +1,19 @@
 class Pet():
+
     def __init__(self, name, age, color):
         self.__name=name
         self.age=age
-        self.__color.color
-    def set
+        self.__color=color
+
+    def set(self):
+        self.__name=input()
+        self.age=input()
+        self.__color=input()
+
+    def get(self):
+        print(self.__name, self.age, self.__color)
+
+cat=Pet("Vasya", 3, "black")
+cat.get()
+cat.set()
+cat.get()
